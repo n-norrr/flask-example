45620832dd3b62aec7f77488d187820194cb5893
n-norrr <norlandbatista@ufl.edu>
2021-03-31 12:46:43 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import socket
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():
-    return 'Hello'
+    return "The host is {}".format(socket.gethostbyname(socket.gethostname()))
 
 
 if __name__ == "__main__":
